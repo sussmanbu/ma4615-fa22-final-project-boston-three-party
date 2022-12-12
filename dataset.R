fc656109aac8a54f8895a6cc25d260c7ad1d14c7
mbhuttor <90875996+mbhuttor@users.noreply.github.com>
2022-12-11 20:27:47 -0500

    Update dataset.R

--- dataset.R
+++ dataset.R
@@ -9,7 +9,7 @@ library(readr)
 
 
 
-## clEAN the data
+## CLEAN THE DATA
 co2_data_clean <- read.csv("dataset/clean_co2.csv")
 
 #write_csv(co2_data_clean, file = here::here("dataset", "clean_co2.csv"))
@@ -38,38 +38,9 @@ cor(co2_data_clean[,-1], co2_data_clean$CO2_emissions_kt)
 #plot of correlation: heatmap
 heatmap(res)
 
-
-
-## BARLEY
-
-
-#crops and co2
-co2_data <- read_csv(here::here("dataset", "CSV_fixed_climate_data.csv"))
-
-## clEAN the data
-co2_data_clean <- co2_data
-
-write_csv(co2_data_clean, file = here::here("dataset", "clean_co2.csv"))
-
-save(co2_data_clean, file = here::here("dataset/clean_co2.RData"))
-co2_data_clean <- co2_data_clean[,-1]
-co2_data_clean <- co2_data_clean %>% type_convert()
-co2_data_clean[co2_data_clean == ".."] = NA
-co2_data_clean[is.na(co2_data_clean)] = 0
-co2_data_clean[co2_data_clean == 0] = NA
-
-co2_data_clean <- co2_data_clean[19:50,]
-
-for(i in 3:ncol(co2_data_clean)){
-  co2_data_clean[,i][is.na(co2_data_clean[,i])] = mean(co2_data_clean[,i],na.rm=TRUE)
-}
-cor(co2_data_clean$CO2_emissions_kt,co2_data_clean$Population_growth_annual_percent,na.rm =TRUE) 
-co2_data_clean$CO2_emissions_kt[co2_data_clean$CO2_emissions_kt==0,] <- mean(co2_data_clean$CO2_emissions_kt, na.rm = TRUE)
-cor(co2_data_clean[3:14],na.rm=T)
-View(co2_data_clean)
+#------------------------------------------------------------------------------------------
 
 ## BARLEY
-
 
 barley_data_3 <- read_csv(here::here("dataset", "barley/crop_p015_t005.csv"),skip = 5)
 barley_data_3<- barley_data_3[-2,3:ncol(barley_data_3)] %>% drop_na(Year) %>% head(-1)
@@ -78,17 +49,17 @@ barley_data_4 <- read_csv(here::here("dataset", "barley/crop_p016_t010.csv"),ski
 barley_data_4<- barley_data_4[-2,3:ncol(barley_data_4)] %>% drop_na(Year) %>% head(-1)
 
 barley_data <- barley_data_3%>%bind_rows(barley_data_4) 
-
+barley_data <- barley_data %>% type_convert()
 
 barley_co2_data <- co2_data_clean %>% left_join(barley_data, by = c("Time" = "Year"))
 
-barley_co2_data <- barley_co2_data %>% filter(CO2_emissions_kt != "..") %>% type_convert()
+#barley_co2_data <- barley_co2_data %>% filter(CO2_emissions_kt != "..") %>% type_convert()
 View(barley_co2_data)
 
 
-write_csv(barely_co2_data, file = here::here("dataset", "barley_co2_data"))
+#write_csv(barely_co2_data, file = here::here("dataset", "barley_co2_data"))
 
-save(barley_co2_data, file = here::here("dataset/barely_co2_data.RData"))
+#save(barley_co2_data, file = here::here("dataset/barely_co2_data.RData"))
 
 
 ggAcf(barley_co2_data$`Area harvested`)  ###use difference 
@@ -100,37 +71,43 @@ ggAcf(diff(barley_co2_data$`Area harvested`)) ## no autocorrelation
 ggAcf(barley_co2_data$CO2_emissions_kt) ##persistent  autocorrelation 
 ggAcf(diff(barley_co2_data$CO2_emissions_kt)) #increase of c02 over time, a good plot
 
-
-barley_co2_data %>% ggplot(aes(x = diff('Area harvested'), y = diff(CO2_emissions_kt))) + geom_point()
+barley_co2_data %>% ggplot(aes(y = `Area harvested`, x = (CO2_emissions_kt))) + geom_point()
+barley_co2_data %>% ggplot(aes(y = `Production`, x = (CO2_emissions_kt))) + geom_point()
+barley_co2_data %>% ggplot(aes(y = `Area planted`, x = (CO2_emissions_kt))) + geom_point()
+barley_co2_data %>% ggplot(aes(y = `Yield per acre`, x = (CO2_emissions_kt))) + geom_point()
 
 diff_df <- data.frame(diff_area = diff(barley_co2_data$`Area harvested`), diff_co2 = diff(barley_co2_data$CO2_emissions_kt) )
-diff_df %>% ggplot(aes(x = diff_area, y = diff_co2)) + geom_point()+ geom_smooth()
-### weak correlation between co2 and harvested
+diff_df %>% ggplot(aes(y = diff_area, x = diff_co2)) + geom_point()+ geom_smooth()
+### weak correlation between difference data, relation seems to be cubic
 
-barley_co2_data %>% ggplot(aes(x = `Area harvested`, y = CO2_emissions_kt)) + geom_point()+ geom_smooth()
-##there is very strong correlation here
+barley_co2_data %>% ggplot(aes(y = `Area harvested`, x = CO2_emissions_kt)) + geom_point()+ geom_smooth()
+##there is very weak correlation
 
 lm1 <- (lm(`Area harvested`~ CO2_emissions_kt, barley_co2_data)) 
+summary(lm1)
 
-lm1_diff <-lm(diff_area ~ diff_co2, diff_df) 
+lm1_diff <-lm(diff_area ~ poly(diff_co2,3), diff_df)
+summary(lm1_diff)
+
+## No significant association between co2 emissions and area harvested
 
 ggAcf(lm1$residuals)
 ggAcf(lm1_diff$residuals)
 
-
+#------------------------------------------------------------------------------------------
 
 ## CORN
 
 corn_data_3 <-read_csv(here::here("dataset", "corn/crop_p031_t024.csv"),skip = 7)
 corn_data_3<- corn_data_3[-2,3:ncol(corn_data_3)] %>% drop_na(Year) %>%filter(nchar(Year)==4)
 colnames(corn_data_3) <- colnames(barley_data_3)
-
+corn_data_3 <- corn_data_3[,1:7]
 View(corn_data_3)
 
 corn_data_4 <- read_csv(here::here("dataset", "corn/crop_p032_t034.csv"),skip = 7)
 corn_data_4<- corn_data_4[-2,3:ncol(corn_data_4)] %>% drop_na(Year) %>%filter(nchar(Year)==4)
 colnames(corn_data_4) <- colnames(barley_data_4)
-
+corn_data_4 <- corn_data_4[,1:7]
 View(corn_data_4)
 
 
@@ -139,9 +116,7 @@ View(corn_data)
 corn_co2_data <- co2_data_clean %>% left_join(corn_data, by = c("Time" = "Year"))
 View(corn_co2_data)
 
-write_csv(corn_co2_data, file = here::here("dataset", "corn_co2_data"))
 
-save(corn_co2_data, file = here::here("dataset/corn_co2_data.RData"))
 
 
 ggAcf(corn_co2_data$`Area harvested`)  ###use difference 
@@ -154,18 +129,22 @@ ggAcf(corn_co2_data$CO2_emissions_kt) ##persistent  autocorrelation
 ggAcf(diff(corn_co2_data$CO2_emissions_kt)) #increase of c02 over time, a good plot
 
 
-corn_co2_data %>% ggplot(aes(x = diff('Area harvested'), y = diff(CO2_emissions_kt))) + geom_point()
+#corn_co2_data %>% ggplot(aes(x = diff(`Area harvested`), y = diff(CO2_emissions_kt))) + geom_point()
 
 diff_df <- data.frame(diff_area = diff(corn_co2_data$`Area harvested`), diff_co2 = diff(corn_co2_data$CO2_emissions_kt) )
 diff_df %>% ggplot(aes(x = diff_area, y = diff_co2)) + geom_point()+ geom_smooth()
 ### weak correlation between co2 and harvested
 
 corn_co2_data %>% ggplot(aes(x = `Area harvested`, y = CO2_emissions_kt)) + geom_point()+ geom_smooth()
-##there is very strong correlation here
+##there is weak correlation here, the relation seems to be cubic
 
-lm1 <- (lm(`Area harvested`~ CO2_emissions_kt, corn_co2_data)) 
+lm1 <- (lm(`Area harvested`~ poly(CO2_emissions_kt,3), corn_co2_data)) 
+summary(lm1)
 
 lm1_diff <-lm(diff_area ~ diff_co2, diff_df) 
+summary(lm1_diff)
+
+# No significant association
 
 ggAcf(lm1$residuals)
 ggAcf(lm1_diff$residuals)
@@ -177,11 +156,7 @@ ggAcf(lm1_diff$residuals)
 
 
 
-
-
-
-
-
+#------------------------------------------------------------------------------------------
 
 
 ## FLAXSEED
@@ -209,6 +184,7 @@ View(flaxseed_data_4)
 flaxseed_data_3 <- flaxseed_data_3 %>% type_convert()
 flaxseed_data <- flaxseed_data_3%>%bind_rows(flaxseed_data_4) 
 View(flaxseed_data)
+
 flaxseed_co2_data <- co2_data_clean %>% left_join(flaxseed_data, by = c("Time" = "Year"))
 
 
@@ -217,9 +193,9 @@ flaxseed_co2_data <- flaxseed_co2_data %>% filter(CO2_emissions_kt != "..") %>% 
 View(flaxseed_co2_data)
 
 
-ggAcf(flaxseed_co2_data$`Area harvested`)  ###use difference 
+ggAcf(flaxseed_co2_data$Area.harvested)  ###use difference 
 
-ggAcf(diff(flaxseed_co2_data$`Area harvested`)) ## no autocorrelation
+ggAcf(diff(flaxseed_co2_data$Area.harvested)) ## no autocorrelation
 
 
 # co2 emission
@@ -227,18 +203,23 @@ ggAcf(flaxseed_co2_data$CO2_emissions_kt) ##persistent  autocorrelation
 ggAcf(diff(flaxseed_co2_data$CO2_emissions_kt)) #increase of c02 over time, a good plot
 
 
-flaxseed_co2_data %>% ggplot(aes(x = diff('Area harvested'), y = diff(CO2_emissions_kt))) + geom_point()
 
-diff_df <- data.frame(diff_area = diff(flaxseed_co2_data$`Area harvested`), diff_co2 = diff(flaxseed_co2_data$CO2_emissions_kt) )
+#flaxseed_co2_data %>% ggplot(aes(x = diff('Area harvested'), y = diff(CO2_emissions_kt))) + geom_point()
+
+diff_df <- data.frame(diff_area = diff(flaxseed_co2_data$Area.harvested), diff_co2 = diff(flaxseed_co2_data$CO2_emissions_kt) )
 diff_df %>% ggplot(aes(x = diff_area, y = diff_co2)) + geom_point()+ geom_smooth()
 ### weak correlation between co2 and harvested
 
-flaxseed_co2_data %>% ggplot(aes(x = `Area harvested`, y = CO2_emissions_kt)) + geom_point()+ geom_smooth()
+flaxseed_co2_data %>% ggplot(aes(x = Area.harvested, y = CO2_emissions_kt)) + geom_point()+ geom_smooth()
 ##there is very strong correlation here
 
-lm2 <- (lm(`Area harvested`~ CO2_emissions_kt, flaxseed_co2_data)) 
+lm2 <- (lm(Area.harvested~ CO2_emissions_kt, flaxseed_co2_data)) 
+summary(lm2)
+#High correlation
 
 lm2_diff <-lm(diff_area ~ diff_co2, diff_df) 
+summary(lm2_diff)
+#differenced data has no correlation, differencing is not required
 
 ggAcf(lm2$residuals)
 ggAcf(lm1_diff$residuals)
@@ -246,56 +227,117 @@ ggAcf(lm1_diff$residuals)
 
 
 
+#------------------------------------------------------------------------------------------
 ## OATS
-
-
-
-
-
-oat_dataset_3 <- read_csv("dataset/oat/crop_p124_t077.csv", 
-                          col_types = cols(`77` = col_skip(), h = col_skip()), 
-                          skip = 5)
-oat_data_3<- oat_dataset_3 %>% drop_na(Year) %>%filter(nchar(Year)==4) %>% drop_na(Year) %>% type_convert()
-
-
+oat_dataset_3<- read_csv(here::here("dataset", "oat/crop_p124_t077.csv"),skip = 5)
+oat_dataset_3<- oat_dataset_3 %>% drop_na(Year) %>%filter(nchar(Year)==4) %>% drop_na(Year) %>% type_convert()
+oat_dataset_3<- oat_dataset_3[-2,3:ncol(oat_dataset_3)] %>% drop_na(Year) %>% head(-1)
+colnames(oat_dataset_3) <- colnames(barley_data_3)
 View(oat_dataset_3)
 
-colnames(oat_data_3) <- colnames(barley_data_3)
-View(oat_dataset_3)
 
-oat_dataset_4 <- read_csv("dataset/oat/crop_p125_t078.csv", 
-                          col_types = cols(`78` = col_skip(), t = col_skip(), 
-                                           u = col_skip(), ...3 = col_double(), 
-                                           `(1,000 acres)...4` = col_double()), 
-                          skip = 7)
-
-
+oat_dataset_4<- read_csv(here::here("dataset", "oat/crop_p125_t078.csv"),skip = 5)
+oat_dataset_4<- oat_dataset_4 %>% drop_na(Year) %>%filter(nchar(Year)==4) %>% drop_na(Year) %>% type_convert()
+oat_dataset_4<- oat_dataset_4[-2,3:ncol(oat_dataset_4)] %>% drop_na(Year) %>% head(-1)
+colnames(oat_dataset_4) <- colnames(barley_data_4)
 View(oat_dataset_4)
 
 
-oat_data <- oat_data_3%>%bind_rows(oat_data_4)
+oat_data <- oat_dataset_3%>%bind_rows(oat_dataset_4) 
+View(oat_data)
+
+oat_co2_data <- co2_data_clean %>% left_join(oat_data, by = c("Time" = "Year"))
+View(oat_co2_data)
+
+oat_co2_data <- oat_co2_data %>% filter(CO2_emissions_kt != "..") %>% type_convert()
+View(oat_co2_data)
 
 
+#--------
+
+ggAcf(oat_co2_data$`Area harvested`)  ###use difference 
+ggAcf(diff(oat_co2_data$`Area harvested`)) ## no autocorrelation
 
 
+# co2 emission
+ggAcf(oat_co2_data$CO2_emissions_kt) 
+ggAcf(diff(oat_co2_data$CO2_emissions_kt)) 
 
 
+oat_co2_data %>% ggplot(aes(x = diff('Area harvested'), y = diff(CO2_emissions_kt))) + geom_point()
 
-## RYE
+diff_df <- data.frame(diff_area = diff(oat_co2_data$`Area harvested`), diff_co2 = diff(oat_co2_data$CO2_emissions_kt) )
+diff_df %>% ggplot(aes(x = diff_area, y = diff_co2)) + geom_point()+ geom_smooth()
 
-rye_data_3 <-read_csv(here::here("dataset", "rye/crop_p170_t101.csv"),skip = 5)
+oat_co2_data %>% ggplot(aes(x = `Area harvested`, y = CO2_emissions_kt)) + geom_point()+ geom_smooth()
+
+
+lm2 <- (lm(`Area harvested`~ CO2_emissions_kt, oat_co2_data)) 
+
+lm2_diff <-lm(diff_area ~ diff_co2, diff_df) 
+
+ggAcf(lm2$residuals)
+ggAcf(lm1_diff$residuals)
+
+oat_co2_data %>% ggplot(aes(y = `Area harvested`, x = (CO2_emissions_kt))) + geom_point()
+oat_co2_data %>% ggplot(aes(y = `Production`, x = (CO2_emissions_kt))) + geom_point()
+oat_co2_data %>% ggplot(aes(y = `Area planted`, x = (CO2_emissions_kt))) + geom_point()
+oat_co2_data %>% ggplot(aes(y = `Yield per acre`, x = (CO2_emissions_kt))) + geom_point()
+
+#------------------------------------------------------------------------------------------
+#RYE
+
+rye_dataset_3<- read_csv(here::here("dataset", "rye/crop_p170_t101.csv"),skip = 5)
+rye_dataset_3<- rye_dataset_3 %>% drop_na(Year) %>%filter(nchar(Year)==4) %>% drop_na(Year) %>% type_convert()
+rye_dataset_3<- rye_dataset_3[-2,3:ncol(oat_dataset_3)] %>% drop_na(Year) %>% head(-1)
+colnames(rye_dataset_3) <- colnames(barley_data_3)
+
 View(rye_dataset_3)
-rye_data_3<- rye_data_3[-2,3:ncol(rye_data_3)] %>% drop_na(Year) %>% type_convert()
 
 
-View(rye_dataset_3)
-
+rye_dataset_4<- read_csv(here::here("dataset", "rye/crop_p171_t102.csv"),skip = 5)
+rye_dataset_4<- rye_dataset_4 %>% drop_na(Year) %>%filter(nchar(Year)==4) %>% drop_na(Year) %>% type_convert()
+rye_dataset_4<- rye_dataset_4[-2,3:ncol(rye_dataset_4)] %>% drop_na(Year) %>% head(-1)
+colnames(rye_dataset_4) <- colnames(barley_data_4)
 
 View(rye_dataset_4)
 
-rye_dataset <- rye_dataset_3%>%bind_rows(rye_dataset_4)
+rye_data <- rye_dataset_3%>%bind_rows(rye_dataset_4) 
+View(rye_data)
+
+rye_co2_data <- co2_data_clean %>% left_join(rye_data, by = c("Time" = "Year"))
+View(rye_co2_data)
+
+rye_co2_data <- rye_co2_data %>% filter(CO2_emissions_kt != "..") %>% type_convert()
+View(rye_co2_data)
+#-------
+
+ggAcf(rye_co2_data$`Area harvested`)  ###use difference 
+ggAcf(diff(rye_co2_data$`Area harvested`)) ## no autocorrelation
 
 
+# co2 emission
+ggAcf(rye_co2_data$CO2_emissions_kt) 
+ggAcf(diff(rye_co2_data$CO2_emissions_kt)) 
 
 
+rye_co2_data %>% ggplot(aes(x = diff('Area harvested'), y = diff(CO2_emissions_kt))) + geom_point()
+
+diff_df <- data.frame(diff_area = diff(rye_co2_data$`Area harvested`), diff_co2 = diff(rye_co2_data$CO2_emissions_kt) )
+diff_df %>% ggplot(aes(x = diff_area, y = diff_co2)) + geom_point()+ geom_smooth()
+
+rye_co2_data %>% ggplot(aes(x = `Area harvested`, y = CO2_emissions_kt)) + geom_point()+ geom_smooth()
+
+
+lm2 <- (lm(`Area harvested`~ CO2_emissions_kt, rye_co2_data)) 
+
+lm2_diff <-lm(diff_area ~ diff_co2, diff_df) 
+
+ggAcf(lm2$residuals)
+ggAcf(lm1_diff$residuals)
+
+rye_co2_data %>% ggplot(aes(y = `Area harvested`, x = (CO2_emissions_kt))) + geom_point()
+rye_co2_data %>% ggplot(aes(y = `Production`, x = (CO2_emissions_kt))) + geom_point()
+rye_co2_data %>% ggplot(aes(y = `Area planted`, x = (CO2_emissions_kt))) + geom_point()
+rye_co2_data %>% ggplot(aes(y = `Yield per acre`, x = (CO2_emissions_kt))) + geom_point()
 
